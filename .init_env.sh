ed38c219158149fb00cd4744daf7f54eed4803a7
smdbh <30342257+smdbh@users.noreply.github.com>
2022-06-25 16:38:48 +0800

    s

--- .init_env.sh
+++ .init_env.sh
@@ -4,10 +4,9 @@ echo "Custom user configurations..."
 
 # apt install
 sudo apt-get update
-sudo apt-get install -y byobu vim zsh  htop zip unzip  net-tools locate
+sudo apt-get install -y byobu htop zip unzip  locate #  net-tools
 
-source ~/.z.sh
-
-# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd ~
+mv .z.sh z.sh
 
 

cc9da12a77440cabaa6f491901732ee72ab7e0f3
smdbh <30342257+smdbh@users.noreply.github.com>
2022-06-25 16:00:11 +0800

    s

--- install.sh
+++ install.sh
@@ -8,10 +8,23 @@ echo "Custom user configurations..."
 
 
 # copy dotfiles
-script_dir=$(dirname "$(readlink -f "$0")")
-ln -s $script_dir/.vimrc ~/.vimrc
-ln -s $script_dir/z.sh ~/z.sh
-ln -s $script_dir/.zshrc ~/.zshrc
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
+
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
+
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
+}
+
+create_symlinks
 
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

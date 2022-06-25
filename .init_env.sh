ab52f2ba1b3afedc909cef75a14e72ec67231674
smdbh <30342257+smdbh@users.noreply.github.com>
2022-06-25 16:50:09 +0800

    tag

--- .init_env.sh
+++ .init_env.sh
@@ -9,4 +9,6 @@ sudo apt-get install -y byobu htop zip unzip  locate #  net-tools
 cd ~
 mv .z.sh z.sh
 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
+echo "finished. OK"
